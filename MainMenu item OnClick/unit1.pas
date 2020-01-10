dcaf0130550d99e4abf35632c20fc7061e34b508
Alexey-t <support@uvviewsoft.com>
2020-01-10 17:10:30 +0300

    test

---
+++ MainMenu item OnClick/unit1.pas
@@ -0,0 +1,55 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem10: TMenuItem;
+    MenuItem11: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    procedure MenuItem1Click(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.MenuItem1Click(Sender: TObject);
+begin
+  caption:= 'ff';
+end;
+
+procedure TForm1.MenuItem2Click(Sender: TObject);
+begin
+  caption:= 'dd';
+end;
+
+end.
+
