dafb55b465698e403108a386194d57a077f101b8
Alexey-t <support@uvviewsoft.com>
2020-02-05 19:51:03 +0300

    up

---
+++ Canvas Font Orientation/unit1.pas
@@ -0,0 +1,51 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Spin;
+
+type
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    PaintBox1: TPaintBox;
+    SpinEdit1: TSpinEdit;
+    procedure PaintBox1Paint(Sender: TObject);
+    procedure SpinEdit1Change(Sender: TObject);
+  private
+    val: integer;
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.PaintBox1Paint(Sender: TObject);
+var
+  cnv: TCanvas;
+begin
+  cnv:=PaintBox1.Canvas;
+  cnv.Font.Color:= clGreen;
+  cnv.Font.Orientation:= val*10;
+  cnv.Font.Style:= [fsItalic];
+  cnv.TextOut(200,100,'Hello World!');
+end;
+
+procedure TForm1.SpinEdit1Change(Sender: TObject);
+begin
+  val:= SpinEdit1.Value;
+  PaintBox1.Invalidate;
+end;
+
+end.
+
