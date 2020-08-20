b1c155761aa1a360b4a98e378c08343821259762
Alexey <support@uvviewsoft.com>
2020-08-20 22:07:26 +0300

    +

---
+++ Controls with accelerator chars/unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls, ComCtrls, Buttons;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    Button1: TButton;
+    Label1: TLabel;
+    StatusBar1: TStatusBar;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
