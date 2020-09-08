0f82d492223ad9384a6dd64b617424796a5ad22a
Alexey <support@uvviewsoft.com>
2020-09-08 11:56:29 +0300

    +

--- Canvas TextOut crossed font/unit1.pas
+++ Canvas TextOut crossed font/unit1.pas
@@ -5,16 +5,16 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Types;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Spin,
+  ComCtrls, Types;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Label1: TLabel;
     ListBox1: TListBox;
-    procedure FormCreate(Sender: TObject);
-    procedure FormPaint(Sender: TObject);
     procedure ListBox1DrawItem(Control: TWinControl; Index: Integer;
       ARect: TRect; State: TOwnerDrawState);
   private
@@ -32,26 +32,32 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.FormPaint(Sender: TObject);
-begin
-  canvas.font.style:= [fsStrikeOut];
-  canvas.font.name:= 'Courier';
-  canvas.font.size:= 15;
-  canvas.textout(20,20,'must be crossed...');
-end;
-
 procedure TForm1.ListBox1DrawItem(Control: TWinControl; Index: Integer;
   ARect: TRect; State: TOwnerDrawState);
+var
+  c: tcanvas;
 begin
-  Listbox1.Canvas.Font.Style:= [fsStrikeOut];
-  Listbox1.Canvas.Font.Color:= clRed;
-  Listbox1.Canvas.Textout(ARect.Left, ARect.Top, Listbox1.Items[Index]);
-end;
-
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-  ListBox1.Items.add('must be crossed, but is not');
-  ListBox1.Items.add('two...');
+  c:= Listbox1.Canvas;
+  c.Font.Color:= clGreen;
+  {
+  bbb
+  iii
+  bbbiii
+  uuu
+  sss
+  uuusss
+  bbbiiiuuusss
+  }
+  case Index of
+    0: c.Font.Style:= [fsBold];
+    1: c.Font.Style:= [fsItalic];
+    2: c.Font.Style:= [fsBold, fsItalic];
+    3: c.Font.Style:= [fsUnderline];
+    4: c.Font.Style:= [fsStrikeOut];
+    5: c.Font.Style:= [fsUnderline, fsStrikeOut];
+    6: c.Font.Style:= [fsBold, fsItalic, fsUnderline, fsStrikeOut];
+  end;
+  c.TextOut(ARect.left, ARect.top, '>> '+Listbox1.Items[Index]);
 end;
 
 end.
