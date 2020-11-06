8cc29ad613e3a171a433e983b230df3b0ab3f4cd
Alexey <support@uvviewsoft.com>
2020-11-06 19:37:38 +0300

    keydown

---
+++ KeyDown key codes/unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ListBox1: TListBox;
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses LCLType, LCLProc, Menus;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+var
+  s: string;
+begin
+  s:= '';
+  if ssAlt in shift then s+='alt ';
+  if ssShift in shift then s+= 'shift ';
+  if ssCtrl in shift then s+= 'ctrl ';
+  listbox1.items.add('key: '+inttostr(Key)+', state: '+s);
+  listbox1.ItemIndex:= ListBox1.items.count-1;
+end;
+
+end.
+
