08c5a6ab5c2366620d79959eb56ad611a5ee15f1
Alexey <support@uvviewsoft.com>
2021-05-11 19:49:19 +0300

    +

---
+++ Flickering on maximize+restore app/unit1.pas
@@ -0,0 +1,37 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ImageList1: TImageList;
+    ListBox1: TListBox;
+    Memo1: TMemo;
+    ToolBar1: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
