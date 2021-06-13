b259fb17a9d2338e0164b9d14c1c6b196271b819
Alexey <support@uvviewsoft.com>
2021-06-14 00:40:43 +0300

    +

---
+++ ListView bad painted grid lines/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ListView1: TListView;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
