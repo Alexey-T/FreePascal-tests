d6ecaa4b3b9727737f6adffe4857ee930bc68b2e
Alextp <aaa5500@ya.ru>
2021-07-16 20:58:56 +0300

    fix

--- Cocoa rendering speed test/unit1.pas
+++ Cocoa rendering speed test/unit1.pas
@@ -45,22 +45,33 @@ var
   C: TCanvas;
   x, y: integer;
   i: integer;
+  chk_Text1,
+  chk_Text2,
+  chk_Rect1,
+  chk_Rect2: boolean;
 begin
   tick:= GetTickCount64;
   C:= PanelMain.Canvas;
+
+  chk_Text1:= CheckTextout.Checked;
+  chk_Text2:= CheckTextoutCl.Checked;
+  chk_Rect1:= CheckRect.Checked;
+  chk_Rect2:= CheckFillRect.Checked;
 
   for i:= 0 to NCount-1 do
   begin
     x:= i div 80 * 100;
     y:= i mod 80 * 10;
 
-    if CheckTextout.Checked then
+    if chk_Text1 then
     begin
-      C.Brush.Style:= bsSolid;
+      C.Font.Color:= clBlack;
+      C.Brush.Color:= clYellow;
+      C.Brush.Style:= bsClear;
       C.TextOut(x, y, 'Simple text string');
     end;
 
-    if CheckTextoutCl.Checked then
+    if chk_Text2 then
     begin
       C.Font.Color:= Random($FFFFFF);
       C.Brush.Color:= Random($FFFFFF);
@@ -68,14 +79,14 @@ begin
       C.TextOut(x, y, 'Colored text string');
     end;
 
-    if CheckRect.Checked then
+    if chk_Rect1 then
     begin
       C.Pen.Color:= Random($FFFFFF);
       C.Brush.Style:= bsClear;
       C.Rectangle(x, y, x+100, y+10);
     end;
 
-    if CheckFillRect.Checked then
+    if chk_Rect2 then
     begin
       C.Pen.Color:= Random($FFFFFF);
       C.Brush.Color:= Random($FFFFFF);
